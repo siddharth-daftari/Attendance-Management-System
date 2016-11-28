4fe0e6a3f50ce371a1de6feb5b9c44fc8aa12e31
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2016-11-27 22:14:29 -0800

    Implement bluetooth server for monitoring attndnce

---
+++ raspberryPi/server.py
@@ -0,0 +1,149 @@
+from bluetooth import SERIAL_PORT_CLASS, SERIAL_PORT_PROFILE
+from WebServerConnector import WebServerConnector
+
+import bluetooth
+import datetime
+import json
+import logging
+import os
+import time
+
+
+class BTServer(object):
+    def __init__(self, port, uuid, backlog):
+        self.port = port
+        self.uuid = uuid
+        self.backlog = backlog
+        self.logFile = 'BTServer%s.log' % int(time.time())
+
+        self._setupLogging()
+        self._createAndBindSocket()
+
+    def _setupLogging(self):
+        self.logger = logging.getLogger('BTser')
+        self.logger.setLevel(logging.DEBUG)
+
+        fh = logging.FileHandler(self.logFile)
+        fh.setLevel(logging.DEBUG)
+
+        # ch = logging.StreamHandler()
+        # ch.setLevel(logging.INFO)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)5s - '
+                                      '%(levelname)7s - %(message)s')
+        fh.setFormatter(formatter)
+        # ch.setFormatter(formatter)
+
+        self.logger.addHandler(fh)
+        # self.logger.addHandler(ch)
+
+    def _closeSocket(self):
+        self.socket.close()
+
+    def _createAndBindSocket(self):
+        self.socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.socket.bind(("", self.port))
+        self.socket.listen(self.backlog)
+
+    def _advertise(self):
+        bluetooth.advertise_service(self.socket, "AquaPiServer",
+                                    service_id=self.uuid,
+                                    service_classes=[self.uuid,
+                                                     SERIAL_PORT_CLASS],
+                                    profiles=[SERIAL_PORT_PROFILE])
+
+    def serve(self, endTime, callback):
+        """
+        Look for incoming connections on the socket till endTime and for each
+        connection, make a call of callback(address). Address is the Bluetooth
+        address of the client.
+        """
+        self._advertise()
+        self.logger.info("Listening for requests...")
+
+        while datetime.datetime.now() < endTime:
+            try:
+                clientsocket, address = self.socket.accept()
+                self.logger.info("Incoming MAC addr: %s" % address[0])
+                data = clientsocket.recv(1024)
+                self.logger.info("Student Name: %s", data)
+                clientsocket.close()
+                callback(address[0])
+            except bluetooth.btcommon.BluetoothError as e:
+                self.logger.error("Bluetooth Error: '%s'" % e.message)
+            self.logger.info("-" * 40)
+
+
+def setupLogging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler('temp.txt')
+    fh.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)5s - '
+                                  '%(levelname)7s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+
+if __name__ == "__main__":
+    # TODO shorten the below lines of code or move them to functions
+    setupLogging()
+
+    with open(os.path.join(os.getcwd(), "serverConf.json")) as fp:
+        config = json.load(fp)
+    webServerCon = WebServerConnector(config["classInfoURL"],
+                                      config["attendanceMakerURL"])
+    while True:
+        # classesInfo = webServerCon.getClassesInfo()
+        # TODO remove the below lines used for demo
+        x = datetime.datetime.now()
+        x += datetime.timedelta(seconds=11)
+        x = x.strftime("%m-%d-%y %H:%M:%S")
+        y = datetime.datetime.now()
+        y += datetime.timedelta(hours=2)
+        y = y.strftime("%m-%d-%y %H:%M:%S")
+        classesInfo = {"CMPE272": {"startTime": x,
+                                   "endTime": y},
+                       "CMPE273": {"startTime": "12-27-16 05:31:05",
+                                   "endTime": "12-27-16 22:31:05"}}
+
+        # Get next class assuming non overlapping class timing
+        now = datetime.datetime.now()
+        nextClass, nextClassStartTime, nextClassEndTime = None, None, None
+        for className in classesInfo:
+            classEndTime = datetime.datetime.strptime(
+                classesInfo[className]['endTime'],
+                "%m-%d-%y %H:%M:%S")
+            classStartTime = datetime.datetime.strptime(
+                classesInfo[className]['startTime'],
+                "%m-%d-%y %H:%M:%S")
+            if classEndTime > now:
+                if nextClass is None or nextClassEndTime > classEndTime:
+                    nextClassStartTime = classStartTime
+                    nextClassEndTime = classEndTime
+                    nextClass = className
+        if nextClass is None:
+            logging.warn("No classes to check attendance for")
+            exit()
+        else:
+            logging.info("Next/Current class start time: %s" % classStartTime)
+            logging.info("Next/Current class end time: %s" % classEndTime)
+        if datetime.datetime.now() < nextClassStartTime:
+            timeDelta = nextClassStartTime - datetime.datetime.now()
+            timeDeltaSecs = timeDelta.total_seconds()
+            logging.error("Sleeping for %s secs" % int(timeDeltaSecs))
+            time.sleep(int(timeDeltaSecs))
+
+        server = BTServer(config["port"], config["uuid"], config["backlog"])
+        server.serve(nextClassEndTime,
+                     lambda stMac: webServerCon.markAttendance(stMac,
+                                                               config["rpMac"],
+                                                               className))
